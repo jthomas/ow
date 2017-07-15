8117fbf6f679d6daadfcd1d1e6165078efe670ca
Traun Leyden <tleyden@couchbase.com>
2017-07-15 07:11:21 -0700

    Code cleanup

--- ow.go
+++ ow.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"log"
 	"io/ioutil"
 )
 
@@ -43,77 +42,51 @@ func sendError(w http.ResponseWriter, code int, cause string) {
 
 func runHandler(w http.ResponseWriter, r *http.Request) {
 
-	log.Printf("runHandler")
-
 	params := Params{}
 
 	defer r.Body.Close()
 	bodyBytes, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Printf("Error reading request body: %v", err)
-		sendError(w, http.StatusBadRequest, err.Error())
+		sendError(w, http.StatusBadRequest, fmt.Sprintf("Error reading request body: %v", err))
 		return
 	}
-	log.Printf("read %d bytes", len(bodyBytes))
-
 
 	if err := json.Unmarshal(bodyBytes, &params); err != nil {
-		log.Printf("Error unmarshaling request: %v", err)
-		sendError(w, http.StatusBadRequest, err.Error())
+		sendError(w, http.StatusBadRequest, fmt.Sprintf("Error unmarshaling request: %v", err))
 		return
 	}
-
-	log.Printf("invoking callback with params: value: %+v", string(params.Value))
 
 	response, err := action(params.Value)
 
 	if err != nil {
-		log.Printf("Error running action: %v", err)
-		sendError(w, http.StatusInternalServerError, err.Error())
+		sendError(w, http.StatusInternalServerError, fmt.Sprintf("Error executing action: %v", err))
 		return
 	}
 
 	b, err := json.Marshal(response)
 	if err != nil {
-		log.Printf("Error marshaling response: %+v. Err: %v", response, err)
-		sendError(w, http.StatusInternalServerError, err.Error())
+		sendError(w, http.StatusInternalServerError, fmt.Sprintf("Error marshaling response: %v", err))
 		return
 	}
 
-	log.Printf("writing %d byte response: %v.  response object: %+v", len(b), string(b), response)
-
-
 	w.Header().Set("Content-Type", "application/json")
+
+	// Set the content-length to avoid using chunked transfer encoding, which will cause OpenWhisk to return error:
+	//   "error": "The action did not produce a valid response and exited unexpectedly."
+	// Workaround source: https://stackoverflow.com/questions/34794647/disable-chunked-transfer-encoding-in-go-without-using-content-length
+	// Related issue: https://github.com/jthomas/ow/issues/2
 	w.Header().Set("Content-Length", fmt.Sprintf("%v", len(b)))
 
 	numBytesWritten, err := w.Write(b)
 	if err != nil {
-		log.Printf("Error writing response:  Err: %v", err)
-		sendError(w, http.StatusInternalServerError, err.Error())
+		sendError(w, http.StatusInternalServerError, fmt.Sprintf("Error writing response: %v", err))
 		return
 	}
 
 	if numBytesWritten != len(b) {
-		sendError(w, http.StatusInternalServerError, fmt.Sprintf("numBytesWritten (%d) != len(b)", numBytesWritten))
+		sendError(w, http.StatusInternalServerError, fmt.Sprintf("Only wrote %d of %d bytes to response", numBytesWritten, len(b)))
 		return
 	}
-
-	log.Printf("No errors and wrote %d bytes", numBytesWritten)
-
-	// w.Header().Set("Content-Type", "application/json")
-
-	// Workaround for nasty bug where OpenWhisk returns error:
-	//   "error": "The action did not produce a valid response and exited unexpectedly."
-	// Workaround source: https://stackoverflow.com/questions/34794647/disable-chunked-transfer-encoding-in-go-without-using-content-length
-	// w.Header().Set("Transfer-Encoding", "identity")
-
-	//enc := json.NewEncoder(w)
-	//if err := enc.Encode(response); err != nil {
-	//	log.Printf("Error encoding json response: %v", err)
-	//	sendError(w, http.StatusInternalServerError, err.Error())
-	//	return
-	//}
-
 
 }
 
